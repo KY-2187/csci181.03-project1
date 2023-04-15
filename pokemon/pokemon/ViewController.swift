cc9ce5416b6b69f178477e771c995300231429ce
KY-2187 <kyrahung1@gmail.com>
2023-04-15 23:29:53 +0800

    Display Pokemon names in UITableView

--- pokemon/pokemon/ViewController.swift
+++ pokemon/pokemon/ViewController.swift
@@ -1,19 +1,46 @@
-//
-//  ViewController.swift
-//  pokemon
-//
-//  Created by Kyra Hung on 4/14/23.
-//
-
 import UIKit
+import PokemonAPI
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var pokemonNames: [String] = []
+    let pokemonAPI = PokemonAPI()
+    private static let reuseIdentifier = "identifier"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.navigationItem.title = "Pokemon (en)"
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: ViewController.reuseIdentifier)
+        
+        for i in 1...151 {
+            pokemonAPI.pokemonService.fetchPokemon("\(i)") { result in
+                switch result {
+                case .success(let pokemon):
+                    self.pokemonNames.append(pokemon.name!.capitalized)
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pokemonNames.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell();
+        cell.textLabel?.text = pokemonNames[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
 }
-
